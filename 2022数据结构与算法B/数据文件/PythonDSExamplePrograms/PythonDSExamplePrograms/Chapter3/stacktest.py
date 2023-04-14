5cbe37ac4815b2d6ec98e058f50cd15b59079bd1
Harry_Potter <joshzhoujm@gmail.com>
2023-04-14 20:06:37 +0800

    第一次更新
    内容包括PDF版教材，北大陈斌python数据结构课的课件以及pythonds数据结构实现的源代码

--- 2022数据结构与算法B/数据文件/PythonDSExamplePrograms/PythonDSExamplePrograms/Chapter3/stacktest.py
+++
@@ -1,6 +0,0 @@
-from pythonds.basic.stack import Stack
-
-s = Stack()
-
-s.push(33)
-print(s.size())
--- 2022数据结构与算法B/数据文件/PythonDSExamplePrograms/PythonDSExamplePrograms/Chapter4/recursivelistsummation.py
+++
@@ -1,7 +0,0 @@
-def listsum(numList):
-     if len(numList) == 1:
-         return numList[0]
-     else:
-         return numList[0] + listsum(numList[1:])
-
-print(listsum([1,3,5,7,9]))
